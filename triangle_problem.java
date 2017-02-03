430ce7751a9ada02efe0d4b22ebea42f224cd748
Kanishk RAMANAND <kramanand@amadeus.com>
2017-02-03 11:28:31 +0530

    First Commit

---
+++ triangle_problem.java
@@ -0,0 +1,42 @@
+package com.kanishk.codegladiator.expert;
+
+public class triangle_problem {
+	
+			public static int[] {
+				//int N = triangle.length;
+				int count = 0,i,j;
+				for(i=1;i<=3;i++)
+
+				{
+
+				for(j=1;j<=(2*i)-1;j++)
+
+				{
+
+				 
+
+				}
+
+				while(j<=3)
+
+				{
+
+				count++;
+
+				j++;
+
+				}
+
+				}
+
+				System.out.println(count);
+
+				return 0;
+
+				}
+		
+
+	}
+
+
+}
